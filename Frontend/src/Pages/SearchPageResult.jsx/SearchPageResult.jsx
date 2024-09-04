37818ad21c3d6393d733e59e3f602a004497a3e0
Umesh Singh <umeshsinghmehta4@gmail.com>
2024-09-05 00:27:10 +0530

    "Added dispatch and axios to SearchPageResult component, modified useEffect to fetch data from API if not available in state, and added loading state with Loader component."

--- Frontend/src/Pages/SearchPageResult.jsx/SearchPageResult.jsx
+++ Frontend/src/Pages/SearchPageResult.jsx/SearchPageResult.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import Navbar from '../../Components/Navbar/Navbar';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import ProductCard from '../../Components/Product Card/ProductCard';
+import axios from 'axios';
+import { setAllProduct } from '../../../Redux/features/product/allproductSlice';
+import Loader from '../../Components/Loader/Loader';
 const SearchPageResult = () => {
     const data = useSelector((state) => state.allProduct.value)
     const location = useLocation();
+    const [loading, setLoading] = useState(false)
+    const dispatch = useDispatch();
     const getQueryParam = () => {
         const query = new URLSearchParams(location.search);
         return query.get('q');
@@ -13,30 +18,48 @@ const SearchPageResult = () => {
     const searchTerm = getQueryParam();
     const [filterOnSearch, setFilterOnSearch] = useState([]);
     useEffect(() => {
-
-        const filteredData = data.filter((item) =>
-            (item.tags.toLowerCase() === searchTerm.toLowerCase()) || (item.productName.toLowerCase() === searchTerm.toLowerCase())
-        );
-        setFilterOnSearch(filteredData);
-
-    }, [searchTerm, data])
+        if (data.length != 0) {
+            const filteredData = data.filter((item) =>
+                (item.tags.toLowerCase() === searchTerm.toLowerCase()) || (item.productName.toLowerCase() === searchTerm.toLowerCase())
+            );
+            return setFilterOnSearch(filteredData);
+        }
+        const fetchProd = async () => {
+            setLoading(true)
+            try {
+                const response = await axios.get('http://localhost:3000/product/allProduct')
+                const fetchData = response.data;
+                dispatch(setAllProduct(fetchData));
+            }
+            catch (err) {
+                console.log(err);
+            }
+            finally {
+                setLoading(false)
+            }
+        }
+        fetchProd();
+    }, [searchTerm, data, dispatch])
     return (
         <div className='searchPageResult'>
             <Navbar />
             <h1>Search Results for {searchTerm}</h1>
-            {filterOnSearch.length > 0 ? (
-                <div className='container'>
-                    {filterOnSearch.map((prod,index) => {
-                        return (
-                            <div key={index}>
-                                <ProductCard details={prod} />
-                            </div>
-                        )
-                    })}
-                </div>
-            ) : (
-                <p>No products found matching "{searchTerm}".</p>
-            )}
+            {loading ? <Loader/> : <div>
+                {filterOnSearch.length > 0 ? (
+                    <div className='container'>
+                        {filterOnSearch.map((prod, index) => {
+                            return (
+                                <div key={index}>
+                                    <ProductCard details={prod} />
+                                </div>
+                            )
+                        })}
+                    </div>
+                ) : (
+                    <p>No products found matching "{searchTerm}".</p>
+                )}
+            </div>}
+
         </div>
     )
 }
