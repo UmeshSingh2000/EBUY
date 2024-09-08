f4c30f8a09d4fea534429449b47b37fcf9fa030c
Umesh Singh <umeshsinghmehta4@gmail.com>
2024-09-08 22:29:31 +0530

    deleted category page and footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,21 +2,13 @@ import React, { useEffect } from 'react'
 import Homepage from './Pages/Hompage/Homepage'
 import Navbar from './Components/Navbar/Navbar'
 import NewArrival from './Pages/NewArrival/NewArrival';
-import Category from './Pages/Category/Category';
-import Footer from './Components/Footer/Footer';
-
-
-
-
-
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Homepage/>
       <NewArrival/>
-      <Category/>
-      <Footer/>
+
     </div>
   )
 }
