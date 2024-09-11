3e472e05e285733bc96e837c40e9d1a52f476158
Umesh Singh <umeshsinghmehta4@gmail.com>
2024-09-12 00:26:36 +0530

    "Added react-router-dom Link component to wrap 'View' button on Homepage"

--- Frontend/src/Pages/Hompage/Homepage.jsx
+++ Frontend/src/Pages/Hompage/Homepage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 const Homepage = () => {
   useEffect(() => {
     document.title = 'Ebuy || Buy Trending Cloths'
@@ -11,7 +12,7 @@ const Homepage = () => {
         }} className='content'>
           <span className='text'>New</span><span className='text'>Mens</span> <span className='text'>Collections</span>
         </div>
-        <button>View</button>
+        <Link to='/allProduct'><button>View</button></Link>
         <p>GET UP TO <span>70% OFF</span></p>
       </div>
     </div>
